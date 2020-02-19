d9cf07de67d1c505b970fcc65e8f7bab8a509146
ivan_khomichkov <zloivan@mail.ru>
2020-02-19 21:53:46 +0300

    Added UI scene Added bunch of scripts

---
+++ Assets/Scripts/ClickSelectController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class ClickSelectController : MonoBehaviour
+ {
+ 
+     [SerializeField] private Camera mainCamera;
+ 
+     public static event Action<Entity> OnSelectedEntityChanged;
+ 
+     public static Entity SelectedEntity { get; private set; }
+ 
+     // Update is called once per frame
+     void Update()
+     {
+         if (Input.GetButtonDown("Fire1"))
+         {
+             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+             Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1f);
+
+             if (Physics.Raycast(ray, out var hitInfo))
+             {
+                 var newEntity = hitInfo.collider.GetComponent<Entity>();
+                 SelectedEntity = newEntity;
+
+                 OnSelectedEntityChanged?.Invoke(newEntity);
+             }
+         }
+
+         if (Input.GetKeyDown(KeyCode.Escape))
+         {
+             SelectedEntity = null;
+             OnSelectedEntityChanged?.Invoke(null);
+         }
+     }
+
+     private void Start()
+     {
+         OnSelectedEntityChanged += OnOnSelectedEntityChangedHandler;
+     }
+
+     private void OnOnSelectedEntityChangedHandler(Entity obj)
+     {
+        
+             if (SelectedEntity != null)
+             {
+                 if (Debug.isDebugBuild) Debug.Log($"<color=purple>Selected: {SelectedEntity.gameObject.name}</color>");
+             }
+             else
+             {
+                 if (Debug.isDebugBuild) Debug.Log("<color=purple>Nothing is selected</color>");
+             }
+     }
+ }
---
+++ Assets/Scripts/UILoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILoader : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (SceneManager.GetSceneByName("UIContainer").isLoaded == false)
+            {
+                SceneManager.LoadScene("UIContainer", LoadSceneMode.Additive);
+            }
+            else
+            {
+                SceneManager.UnloadSceneAsync("UIContainer");
+            }
+        }
+    }
+}
